79465e4abf7868c2b674c638a7860921c1fe9556
Gizen <hoangdaheodaheo@gmail.com>
2021-06-03 13:48:46 +0700

    modify resultWarning

--- src/model/Result/GetResultWarning.model.js
+++ src/model/Result/GetResultWarning.model.js
@@ -1,6 +1,6 @@
 var dataString = "";
 module.exports = function (db, callback) {
-  let sql = `SELECT user.id,user.NAME ,temperature,user.sex,DATE_FORMAT(createAtTime,'%M %d %Y %H:%i:%s')  FROM result INNER JOIN user ON user.id = result.idUser where isQualified = true and temperature >= 37`;
+  let sql = `SELECT user.id,user.NAME ,temperature,user.sex,DATE_FORMAT(createAtTime,'%Y-%m-%d %H:%i:%s') as time  FROM result INNER JOIN user ON user.id = result.idUser where isQualified = true and temperature >= 37`;
   db.query(sql, function (err, results, fields) {
     if (err) {
       throw err;
